95eda86902b276362be0bc16ef9221d9cedc89f9
Hallelujah Shketa <1694405@students.wits.ac.za>
2020-04-30 20:31:38 +0200

    last commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// const path = require('path')
+const express = require('express')
+const app = express()
+// const mainRouter = express.Router()
+// mainRouter.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+// mainRouter.get('/about', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'views', 'about.html'))
+// })
+
+const mod = require('./mainRouter')
+app.use(mod)
+app.listen(3000)
+console.log('Express server running on port 3000')
